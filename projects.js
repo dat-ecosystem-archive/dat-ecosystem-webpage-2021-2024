c8651144e3d8deac83f5a95d9c9d6f4acc82fe5c
mimi-uxui-dev <uxui.mimi@gmail.com>
2022-07-09 14:35:08 +0200

    projects page done

--- projects.js
+++ projects.js
@@ -2,51 +2,428 @@
 const projects = [
     {
         id: 1,
-        img: 'assets/agregore.png',
+        img: 'assets/images/agregore.png',
         title: 'Agregore',
         text: 'An experimental web browser for mixing together p2p and web3 protocols.', 
         socials: [
             {
-                img: 'assets/website.png',
+                img: 'assets/images/website.png',
                 link: ''
             },
             {
-                img: 'assets/icons8-github-100.png',
+                img: 'assets/images/HOME/icons8-github-100.png',
                 link: ''
             },
             {
-                img: 'assets/icons8-discord-new-100.png',
+                img: 'assets/images/HOME/icons8-discord-new-100.png',
                 link: ''
             },
             {
-                img: 'assets/icons8-twitter-100.png',
+                img: 'assets/images/HOME/icons8-twitter-100.png',
                 link: ''
             }
         ], 
         tags: ['Node JS', 'P2P Browser']
+    },
+    {
+        id: 2,
+        img: 'assets/images/projects/ara.png',
+        title: 'Ara',
+        text: 'Ara is a set of decentralized, open source software tools that handle real-world, user-level functionality in online identity, content distribution, and rights management.', 
+        socials: [
+            {
+                img: 'assets/images/website.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-github-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-discord-new-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-twitter-100.png',
+                link: ''
+            }
+        ], 
+        tags: ['Node JS', 'Music']
+    },
+    {
+        id: 3,
+        img: 'assets/images/projects/arso.png',
+        title: 'Arso',
+        text: 'Arso builds tools for decentralized archives of community media, currently developing Sonar, a p2p database and search engine.', 
+        socials: [
+            {
+                img: 'assets/images/website.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-github-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-discord-new-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-twitter-100.png',
+                link: ''
+            }
+        ], 
+        tags: ['Node JS', 'P2P Search']
+    },
+    {
+        id: 4,
+        img: 'assets/images/projects/beaker.png',
+        title: 'Beaker',
+        text: 'Beaker is an experimental browser for exploring and building the peer-to-peer Web.', 
+        socials: [
+            {
+                img: 'assets/images/website.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-github-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-discord-new-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-twitter-100.png',
+                link: ''
+            }
+        ], 
+        tags: ['Node JS', 'P2P Browser']
+    },
+    {
+        id: 5,
+        img: 'assets/images/projects/cabal.svg',
+        title: 'Cabal',
+        text: 'Cabal is an experimental p2p community chat platform, focusing on group chat in channels.', 
+        socials: [
+            {
+                img: 'assets/images/website.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-github-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-discord-new-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-twitter-100.png',
+                link: ''
+            }
+        ], 
+        tags: ['Node JS', 'P2P Chat']
+    },
+    {
+        id: 6,
+        img: 'assets/images/projects/Consento.png',
+        title: 'Consento',
+        text: 'Data privacy for humans, by humans.', 
+        socials: [
+            {
+                img: 'assets/images/website.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-github-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-discord-new-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-twitter-100.png',
+                link: ''
+            }
+        ], 
+        tags: ['Node JS', 'Key Management']
+    },
+    {
+        id: 7,
+        img: 'assets/images/projects/datcxx.png',
+        title: 'Hypercore CXX',
+        text: 'C++ implementation of Hypercore protocol.', 
+        socials: [
+            {
+                img: 'assets/images/website.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-github-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-discord-new-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-twitter-100.png',
+                link: ''
+            }
+        ], 
+        tags: ['C++', 'Protocol']
+    },
+    {
+        id: 8,
+        img: 'assets/images/projects/datdot.png',
+        title: 'Dat Dot',
+        text: 'Autonomous hosting network for p2p data systems.', 
+        socials: [
+            {
+                img: 'assets/images/website.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-github-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-discord-new-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-twitter-100.png',
+                link: ''
+            }
+        ], 
+        tags: ['Node JS', 'Rust', 'P2P Hosting']
+    },
+    {
+        id: 9,
+        img: 'assets/images/projects/datrs.png',
+        title: 'DatRS',
+        text: 'Rust implementation of the Hyper protocols.', 
+        socials: [
+            {
+                img: 'assets/images/website.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-github-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-discord-new-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-twitter-100.png',
+                link: ''
+            }
+        ], 
+        tags: [ 'Rust', 'Protocol']
+    },
+    {
+        id: 10,
+        img: 'assets/images/projects/pico.png',
+        title: 'Pico Stack',
+        text: 'Web3 Framework, 0% Backend, 10000% Frontend.', 
+        socials: [
+            {
+                img: 'assets/images/website.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-github-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-discord-new-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-twitter-100.png',
+                link: ''
+            }
+        ], 
+        tags: [ 'Node JS', 'Framework']
+    },
+    {
+        id: 11,
+        img: 'assets/images/projects/hypercore-protocol.png',
+        title: 'Hypercore protocol',
+        text: 'A world of distributed data structures', 
+        socials: [
+            {
+                img: 'assets/images/website.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-github-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-discord-new-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-twitter-100.png',
+                link: ''
+            }
+        ], 
+        tags: [ 'Node JS', 'Protocol']
+    },
+    {
+        id: 13,
+        img: 'assets/images/projects/peermaps.svg',
+        title: 'Peermaps',
+        text: 'Peermaps is a distributed, offline-friendly alternative to commercial map providers such as google maps.', 
+        socials: [
+            {
+                img: 'assets/images/website.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-github-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-discord-new-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-twitter-100.png',
+                link: ''
+            }
+        ], 
+        tags: [ 'Node JS', 'Rust', 'P2P Maps']
+    },
+    {
+        id: 14,
+        img: 'assets/images/projects/sher.png',
+        title: 'Sher',
+        text: 'A decentralized live streaming audio platform.', 
+        socials: [
+            {
+                img: 'assets/images/website.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-github-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-discord-new-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-twitter-100.png',
+                link: ''
+            }
+        ], 
+        tags: [ 'Node JS', 'Studio']
+    },
+    {
+        id: 16,
+        img: 'assets/images/projects/socket-supply.png',
+        title: 'Socket Supply',
+        text: 'Deploy everywhere. Peer to peer and cloud computing.', 
+        socials: [
+            {
+                img: 'assets/images/website.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-github-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-discord-new-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-twitter-100.png',
+                link: ''
+            }
+        ], 
+        tags: [ 'Node JS', 'P2P Computing']
+    },
+    {
+        id: 17,
+        img: 'assets/images/projects/telios.png',
+        title: 'Telios',
+        text: 'Peer-to-Peer Decentralized Email for Privacy and Security', 
+        socials: [
+            {
+                img: 'assets/images/website.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-github-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-discord-new-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-twitter-100.png',
+                link: ''
+            }
+        ], 
+        tags: [ 'Node JS', 'P2P Mail']
+    },
+    {
+        id: 18,
+        img: 'assets/images/projects/tradle.png',
+        title: 'Tradle',
+        text: 'Building a trust provisioning network.', 
+        socials: [
+            {
+                img: 'assets/images/website.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-github-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-discord-new-100.png',
+                link: ''
+            },
+            {
+                img: 'assets/images/HOME/icons8-twitter-100.png',
+                link: ''
+            }
+        ], 
+        tags: [ 'Node JS']
     }
 ]
-
+ 
 let projectsContainer = document.getElementById('projectsContainer')
 
 let project = projects.map(e => `<div class="project_card">
-    <img
-        class="project_img"
-        src="${e.img}"
-        alt=""
-    />
+    <div class='project_card__img'  >   
+        <img
+            class="project_img"
+            src="${e.img}"
+            alt=""
+        />
+    </div>   
     <div>
-    <h6 class="project_title">${e.title}</h6>
-    <p class="project_text">
-        ${e.text}
-    </p>
-    <div class="project_sm">
-        ${e.socials.map(s => `<a href='${s.link}'><img src="${s.img}" alt="" /></a>`).join('')}
-    </div>
-    <div class="project_tags">
-        ${e.tags.map(tag => `<p class="project_tag">${tag}</p>`).join('')}
-        
-    </div>
+        <h6 class="project_title">${e.title}</h6>
+        <p class="project_text">
+            ${e.text}
+        </p>
+        <div class="project_sm">
+            ${e.socials.map(s => `<a href='${s.link}'><img src="${s.img}" alt="" /></a>`).join('')}
+        </div>
+        <div class="project_tags">
+            ${e.tags.map(tag => `<p class="project_tag">${tag}</p>`).join('')}
+            
+        </div>
     </div>
 </div>`).join('')
 
